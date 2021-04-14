4d7c2592fea7cf647f0cdeeea210dabcc74bdc97
Micah <mrusher@gmail.com>
2021-04-14 16:02:09 -0500

    Can now edit, add, and remove all fields

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experiments: object[] = [
+    {name: "Mars soil sample"},
+    {name: "Plant growth in habitat"},
+    {name: "Human bone density"}
+  ];
+
+  testBeingEdited: object= null;
+
+  save(name: string, test: object) {
+    test['name'] = name;
+    this.testBeingEdited = null;
+    }
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  add(testName: string) {
+    this.experiments.push({name: testName});
+  }
+
+  remove(test: object) {
+    let index = this.experiments.indexOf(test);
+    this.experiments.splice(index, 1);
+  }
+
+  edit(test: object){
+    this.testBeingEdited = test;
+  }
+  
 
 }
